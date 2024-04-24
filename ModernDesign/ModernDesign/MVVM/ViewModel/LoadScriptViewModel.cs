ea0411b54235442efd79a3d8277480f42cb8f22a
Biraj Thapa <bth030@email.latech.edu>
2024-04-23 21:29:54 -0500

    Adjusted UI and Load Script Functionality

---
+++ ModernDesign/ModernDesign/MVVM/ViewModel/LoadScriptViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ModernDesign.Core;
+using Microsoft.Win32;
+using System.IO;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    class LoadScriptViewModel : ObservableObject
+    {
+        private string _scriptContent;
+
+        public string ScriptContent
+        {
+            get => _scriptContent;
+            set
+            {
+                _scriptContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand LoadScriptCommand { get; set; }
+
+        public LoadScriptViewModel()
+        {
+            LoadScriptCommand = new RelayCommand(o => LoadScript());
+        }
+
+        private void LoadScript()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Script files (*.txt;*.cs;*.js)|*.txt;*.cs;*.js|All files (*.*)|*.*",
+                DefaultExt = "txt"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ScriptContent = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+    }
+}
