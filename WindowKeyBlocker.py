23453039d9b611684c97b181b092ddb9e32376b4
joshwilson1212 <joshuawilson2001@gmail.com>
2024-04-26 00:53:12 -0500

    removed the past under tab and  completed the WindowKeyBlocker
    have fully completed the window keyblocker. there are still some know issues that will be resolved soon

--- WindowKeyBlocker.py
+++ WindowKeyBlocker.py
@@ -2,6 +2,8 @@ import Reader
 import time
 import pyautogui
 import keyboard
+from win32api import GetKeyState 
+from win32con import VK_NUMLOCK
 
 #this program is responsible for just blocking the spevified windows keys and allowing the combo on keys
 #function used to format the keys to send to the Hotkeyfunction of 
@@ -22,79 +24,82 @@ def formatKeys(hotkeys):
     return KeyFinal
 
 def HotkeyAction(keys,PastInfo,HighlightInfo,StartCurosrInfo,mouseInfo,MoveAfterTab,HighlightonTab):
-    esc_pressed = False  # Flag variable to track if 'esc' key is pressed
+    #adds a flag and tabindex to keep track of the escape key to exit the program and to keep track of tabs,also a numlock flag
+    esc_pressed = False
     tabindex = 0
+    wasNumlockDisabled = False
+
     def my_function(PastInfo,HighlightInfo,StartCurosrInfo,mouseInfo,MoveAfterTab,HighlightonTab):
-        print("HotKeyPressed")
+        #buffer time just in case, might cause errors if no buffer
         time.sleep(.5)
-        print(PastInfo)
-        print(HighlightInfo)
-        print(StartCurosrInfo)
-        print(mouseInfo)
-        print(MoveAfterTab)
-        print(HighlightonTab)
-        pyautogui.typewrite(PastInfo[0])
-        time.sleep(.5)
-        for x in range((len(PastInfo[0]))- int(StartCurosrInfo[0])):
-            pyautogui.press("left",interval=.01)
+
+        #we check the state of the numlock key, as is causes weird interactions with the shift keys, if the numlock is on we turn it off and take a note that it was done
+        if GetKeyState(VK_NUMLOCK) == 1:
+            pyautogui.press('numlock')
+            wasNumlockDisabled = True
+
+        #reads from the past info of the hotkey and moves it to starting position
+        keyboard.write(PastInfo[0])
+        pyautogui.press("left",presses=(len(PastInfo[0]))- int(StartCurosrInfo[0]),interval=.01)
         
+        #selects the initial variable
         keyboard.press('left shift')
         keyboard.press('right shift')
         pyautogui.press("right",presses=int(HighlightInfo[0]),interval=.01)
         keyboard.release('right shift')
         keyboard.release('left shift')
 
-        keyboard.add_hotkey('tab',lambda:tab_pressed(MoveAfterTab,HighlightonTab),suppress=True,trigger_on_release=True)
-        
-        #for x in MoveAfterTab:
-            #for NumberOfPresses in range(int(x[0])): 
-            #    pyautogui.press("right",interval=.01)
-        print("function end")    
+        #temporarly adds tab to a hot key to call a function to hook it and read from the rest of the variables
+        keyboard.add_hotkey('tab',lambda:tab_pressed(MoveAfterTab,HighlightonTab,wasNumlockDisabled),suppress=True,trigger_on_release=True)
+
+    #this function is a hot key for the esc key to exit the program
     def my_exit():
         global esc_pressed
         print("Quitting...")
         esc_pressed = True
-    def tab_pressed(MoveAfterTab,HighlightonTab):
-        nonlocal tabindex
-        time.sleep(.5)
-        print("tab pressed")
-        for x in range(int(MoveAfterTab[tabindex][0])):
-            pyautogui.press("right",interval=.01)
-        tabindex+=1
 
-    # Register hotkeys for 'ctrl+alt+h' and 'esc'
+    #this function is used to move the key cursor to the proper location after initial selection
+    def tab_pressed(MoveAfterTab,HighlightonTab,wasNumlockDisabled):
+        nonlocal tabindex
+
+        #checks the length of the tab variable to make sure to only use this if there are tabs left, else we unhook tab to free is from the program.
+        if tabindex < len(MoveAfterTab):
+            time.sleep(.5)
+            pyautogui.press("right",presses=int(MoveAfterTab[tabindex][0]),interval=.01)
+
+            keyboard.press('left shift')
+            keyboard.press('right shift')
+            pyautogui.press("right",presses=int(HighlightonTab[tabindex][0]),interval=.01)
+            keyboard.release('right shift')
+            keyboard.release('left shift')
+            tabindex+=1
+        #after all the tabs have been gone through it unhooks tab and checks if we diabled numlock at the start and will re-enable it 
+        else:
+            keyboard.remove_hotkey('tab')
+            if wasNumlockDisabled:
+                pyautogui.press('numlock')  
+
+    #initial registaion of our hotkeys and our esc to quit the program
+    #the only known limitaion i know of right now is that for some reason adding suppress=True and trigger_on_release=True do not function properly in this area, even though it works up top
     keyboard.add_hotkey(keys, lambda: my_function(PastInfo,HighlightInfo,StartCurosrInfo,mouseInfo,MoveAfterTab,HighlightonTab))
-    
     keyboard.add_hotkey('esc', my_exit)
     
 
+    #a try funtion that will hold a subprocces and see if esc was pressed, if Ctrl+c is click it catches the exception and quits the program and unhooks all keys
     try:
         while True:
-            
             keyboard.wait('esc')
-            break  # Exit the while loop when 'esc' is pressed
+            break
     except KeyboardInterrupt:
-        pass  # Handle Ctrl+C gracefully
+        pass  
     finally:
-        keyboard.unhook_all()  # Ensure hotkeys are unhooked before exiting 
+        keyboard.unhook_all()
 
-
-file = "C:/Users/joshu/Desktop/git/CapstoneRepo/Test Hotkey Files/pseudolang2.txt"
-#file = "C:/Users/joshu/Documents/GitHub/CapstoneRepo/Test Hotkey Files/pseudolang2.txt"
-
-tree = Reader.createCommandTree(file)
-info = tree.solve()
-HKList = tree.getHKList()
-#print(f"Prints Hotkeys In List format{HKList}")
-keys = formatKeys(HKList)
-#print(f"Prints keys{repr(keys)}")
-
-
-
+#unpacks the tuples from the main program,
+#KNOWN ISSUE: some tuples get left with weird commas at the end, will come pack to unpack more effectivly
 def unpacktuple(commands):
     tabInfo = []
     for i in commands:
-        #print(f"unpacked Commands:{i}")
         if i[0] == "Paste":
             PastInfo = i[1:]
         elif i[0] == "Highlight":
@@ -110,36 +115,48 @@ def unpacktuple(commands):
             print("error with tuple")
     return PastInfo, HighlightInfo, StartCurosrInfo, mouseInfo,tabInfo
 
-    #past will type out the information in the past section
-    # on hotket press, the information is pasted from the past tuple, we take the length of the past string and move the cursor back to the start of the
-    #pasted string, then we read starcursorinfor to move the cursor to the first replacing variable. then you read the highligh info to get the length of
-    # the first variable and highligh out to the end of its legth, then the user will type in what ever they want. after they replace it the cursor will
-    #be at the end of it then we read the move cursor for the next variable and repeat, after every move cursor info we read highlight infor and keep
-    # reapeating untill there is no more movecursor info.
-
-    #need to unpack new tab data that oragnizes the highligh and move cursor info. 
-
+#unpacks the second layer of tuples labeled as tab
+#same issue as above
 def processTabs(tabList):
     movecursorInfo = []
     highlightInfoTab = []
     for tabs in tabList:
-        #print(tabs)
-        #reads the Move Cursor info
         movecursorInfo.append(tabs[0][1:])
-
-        #reads the highlight action after a tab press
         highlightInfoTab.append(tabs[1][1:])
 
     return movecursorInfo,highlightInfoTab
 
 
+
+#KNOW ISSUE: eventually this should read the file from the sys.argv cmd line
+#file = "C:/Users/joshu/Desktop/git/CapstoneRepo/Test Hotkey Files/pseudolang2.txt"
+file = "C:/Users/joshu/Documents/GitHub/CapstoneRepo/Test Hotkey Files/pseudolang2.txt"
+
+#creates the tree, solves it, takes the keys, formats keys, and takes the return of tree.solve() and sends them to be unpacked
+tree = Reader.createCommandTree(file)
+info = tree.solve()
+HKList = tree.getHKList()
+keys = formatKeys(HKList)
+
+#unpacks the tuples and unpacks the tabs information to be stored in their individual variables for processing
 PastInfo,HighlightInfo,StartCurosrInfo,mouseInfo,tabInfo =  unpacktuple(info)
-
-
 MoveAfterTab,HighlightonTab = processTabs(tabInfo)
 
-#print(MoveAfterTab)
-#print(HighlightonTab)
+#starts the main program
+#HotkeyAction(keys,PastInfo,HighlightInfo,StartCurosrInfo,mouseInfo,MoveAfterTab,HighlightonTab)
 
 
-HotkeyAction(keys,PastInfo,HighlightInfo,StartCurosrInfo,mouseInfo,MoveAfterTab,HighlightonTab)
+#below is a snipit of code that will return the active window
+'''
+import pygetwindow as gw
+def get_active_window_title():
+    active_window = gw.getActiveWindow()
+    if active_window is not None:
+        return active_window.title
+    else:
+        return "No active window found"
+
+time.sleep(10)
+active_window_title = get_active_window_title()
+print("Active Window Title:", active_window_title)
+'''
